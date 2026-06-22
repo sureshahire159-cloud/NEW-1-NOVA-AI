73a785a9396495d5a14b4abc6359ee8ccfc169e7
sureshahire159-cloud <sureshahire159@gmail.com>
2026-06-22 16:08:12 +0530

    Update generate-strategy.ts

--- api/generate-strategy.ts
+++ api/generate-strategy.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { runNvidiaAI, extractJSON } from './_utils';
+import { runNvidiaAI } from './utils';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
